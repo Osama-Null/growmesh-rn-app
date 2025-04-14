9c310ab97f980a712d75aa2b75e570428f3400c5
bderalq <balqallaf12@gmail.com>
2025-04-14 14:14:48 +0300

    register integration done

--- src/navigations/AuthNavigation/AuthNavigation.js
+++ src/navigations/AuthNavigation/AuthNavigation.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../../screens/Auth/Login";
+import Register from "../../screens/Auth/Register";
+import Welcome from "../../screens/Auth/Welcome";
 
 const Stack = createNativeStackNavigator();
 
 const AuthNavigator = () => {
   return (
     <Stack.Navigator >
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="Login" component={Login} />
-      {/* <Stack.Screen name="Register" component={Register} /> */}
     </Stack.Navigator>
   );
 };
