6480c9635a7c39d42b4b51823d5042eca932d11a
alene <alene@ABDALLAH>
2025-04-21 11:26:25 +0300

    history

---
+++ src/screens/History/Transaction.js
@@ -0,0 +1,246 @@
+import React from "react";
+import { SafeAreaView, View, ScrollView, Text, Image, } from "react-native";
+export default (props) => {
+	return (
+		<SafeAreaView 
+			style={{
+				flex: 1,
+				backgroundColor: "#FFFFFF",
+			}}>
+			<ScrollView  
+				style={{
+					flex: 1,
+					backgroundColor: "#FFFFFF",
+				}}>
+				<View 
+					style={{
+						marginTop: 171,
+						marginBottom: 286,
+						marginHorizontal: 16,
+					}}>
+					<View 
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							backgroundColor: "#1E1E1E1A",
+							borderRadius: 14,
+							paddingVertical: 6,
+							paddingLeft: 12,
+							marginBottom: 20,
+						}}>
+						<Text 
+							style={{
+								color: "#1E1E1E",
+								fontSize: 16,
+								textAlign: "center",
+								flex: 1,
+							}}>
+							{"25 Jan 2019"}
+						</Text>
+						<Text 
+							style={{
+								color: "#000000",
+								fontSize: 16,
+								marginRight: 24,
+							}}>
+							{"KWD 24.44"}
+						</Text>
+						<Image
+							source = {{uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/mGvQz95RGi/2078mwis_expires_30_days.png"}} 
+							resizeMode = {"stretch"}
+							style={{
+								borderRadius: 14,
+								width: 30,
+								height: 48,
+							}}
+						/>
+					</View>
+					<View 
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							backgroundColor: "#1E1E1E1A",
+							borderRadius: 14,
+							paddingVertical: 6,
+							paddingLeft: 12,
+							marginBottom: 20,
+						}}>
+						<Text 
+							style={{
+								color: "#1E1E1E",
+								fontSize: 16,
+								textAlign: "center",
+								flex: 1,
+							}}>
+							{"25 Jan 2019"}
+						</Text>
+						<Text 
+							style={{
+								color: "#000000",
+								fontSize: 16,
+								marginRight: 24,
+							}}>
+							{"KWD 24.44"}
+						</Text>
+						<Image
+							source = {{uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/mGvQz95RGi/mxp43lfc_expires_30_days.png"}} 
+							resizeMode = {"stretch"}
+							style={{
+								borderRadius: 14,
+								width: 30,
+								height: 48,
+							}}
+						/>
+					</View>
+					<View 
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							backgroundColor: "#1E1E1E1A",
+							borderRadius: 14,
+							paddingVertical: 6,
+							paddingLeft: 12,
+							marginBottom: 20,
+						}}>
+						<Text 
+							style={{
+								color: "#1E1E1E",
+								fontSize: 16,
+								textAlign: "center",
+								flex: 1,
+							}}>
+							{"25 Jan 2019"}
+						</Text>
+						<Text 
+							style={{
+								color: "#000000",
+								fontSize: 16,
+								marginRight: 24,
+							}}>
+							{"KWD 24.44"}
+						</Text>
+						<Image
+							source = {{uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/mGvQz95RGi/2q0zrojb_expires_30_days.png"}} 
+							resizeMode = {"stretch"}
+							style={{
+								borderRadius: 14,
+								width: 30,
+								height: 48,
+							}}
+						/>
+					</View>
+					<View 
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							backgroundColor: "#1E1E1E1A",
+							borderRadius: 14,
+							paddingVertical: 6,
+							paddingLeft: 12,
+							marginBottom: 20,
+						}}>
+						<Text 
+							style={{
+								color: "#1E1E1E",
+								fontSize: 16,
+								textAlign: "center",
+								flex: 1,
+							}}>
+							{"25 Jan 2019"}
+						</Text>
+						<Text 
+							style={{
+								color: "#000000",
+								fontSize: 16,
+								marginRight: 24,
+							}}>
+							{"KWD 24.44"}
+						</Text>
+						<Image
+							source = {{uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/mGvQz95RGi/oqwa94wh_expires_30_days.png"}} 
+							resizeMode = {"stretch"}
+							style={{
+								borderRadius: 14,
+								width: 30,
+								height: 48,
+							}}
+						/>
+					</View>
+					<View 
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							backgroundColor: "#1E1E1E1A",
+							borderRadius: 14,
+							paddingVertical: 6,
+							paddingLeft: 12,
+							marginBottom: 20,
+						}}>
+						<Text 
+							style={{
+								color: "#1E1E1E",
+								fontSize: 16,
+								textAlign: "center",
+								flex: 1,
+							}}>
+							{"25 Jan 2019"}
+						</Text>
+						<Text 
+							style={{
+								color: "#000000",
+								fontSize: 16,
+								marginRight: 24,
+							}}>
+							{"KWD 24.44"}
+						</Text>
+						<Image
+							source = {{uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/mGvQz95RGi/likbxf1w_expires_30_days.png"}} 
+							resizeMode = {"stretch"}
+							style={{
+								borderRadius: 14,
+								width: 30,
+								height: 48,
+							}}
+						/>
+					</View>
+					<View 
+						style={{
+							flexDirection: "row",
+							alignItems: "center",
+							backgroundColor: "#1E1E1E1A",
+							borderRadius: 14,
+							paddingVertical: 6,
+							paddingLeft: 12,
+						}}>
+						<Text 
+							style={{
+								color: "#1E1E1E",
+								fontSize: 16,
+								textAlign: "center",
+								flex: 1,
+							}}>
+							{"25 Jan 2019"}
+						</Text>
+						<Text 
+							style={{
+								color: "#000000",
+								fontSize: 16,
+								marginRight: 24,
+							}}>
+							{"KWD 24.44"}
+						</Text>
+						<Image
+							source = {{uri: "https://storage.googleapis.com/tagjs-prod.appspot.com/v1/mGvQz95RGi/buq0guu5_expires_30_days.png"}} 
+							resizeMode = {"stretch"}
+							style={{
+								borderRadius: 14,
+								width: 30,
+								height: 48,
+							}}
+						/>
+					</View>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	)
+}
